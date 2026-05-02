8c68d102496f2c8146a5f65e07bd20f951d16e5f
schwarzwhite <donskoy.prince@mail.ru>
2026-05-02 16:00:19 +0300

    softmax

--- CS231N/assignment1/cs231n/classifiers/linear_classifier.py
+++ CS231N/assignment1/cs231n/classifiers/linear_classifier.py
@@ -53,33 +53,13 @@ class LinearClassifier(object):
         for it in range(num_iters):
             X_batch = None
             y_batch = None
-
-            #########################################################################
-            # TODO:                                                                 #
-            # Sample batch_size elements from the training data and their           #
-            # corresponding labels to use in this round of gradient descent.        #
-            # Store the data in X_batch and their corresponding labels in           #
-            # y_batch; after sampling X_batch should have shape (batch_size, dim)   #
-            # and y_batch should have shape (batch_size,)                           #
-            #                                                                       #
-            # Hint: Use np.random.choice to generate indices. Sampling with         #
-            # replacement is faster than sampling without replacement.              #
-            #########################################################################
             
             idx = np.random.choice(num_train, batch_size, replace=True)
             X_batch = X[idx]
             y_batch = y[idx]
-
-            # evaluate loss and gradient
+            
             loss, grad = self.loss(X_batch, y_batch, reg)
             loss_history.append(loss)
-
-            # perform parameter update
-            #########################################################################
-            # TODO:                                                                 #
-            # Update the weights using the gradient and the learning rate.          #
-            #########################################################################
-            
             self.W -= learning_rate * grad
 
 
@@ -103,10 +83,6 @@ class LinearClassifier(object):
           class.
         """
         y_pred = np.zeros(X.shape[0])
-        ###########################################################################
-        # TODO:                                                                   #
-        # Implement this method. Store the predicted labels in y_pred.            #
-        ###########################################################################
         scores = X.dot(self.W)
         y_pred = np.argmax(scores, axis=1)
 
